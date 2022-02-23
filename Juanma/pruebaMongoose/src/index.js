33523316272c22527adf6106cbbeeb80308ded3c
JuanmaX4 <74423956+JuanmaX4@users.noreply.github.com>
2022-02-23 15:52:35 +0100

    Update Alpha 0.412

---
+++ Juanma/pruebaMongoose/src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+// Initiliazations
+const app = express();
+
+// Settings
+app.set('port', 3000)
+// Middlewares
+
+// Global Variable
+
+// Routes
+
+// Static Files
+
+// Server is listenning
+
