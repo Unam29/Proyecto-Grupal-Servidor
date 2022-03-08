72c2cebb17b6cdb49f1717a6584f5c142274005e
Seevii16 <sevillanito435@gmail.com>
2022-03-08 01:52:41 +0100

    Implementacion NodeMailer

--- mongoose/src/controllers/pagos.js
+++ mongoose/src/controllers/pagos.js
@@ -1,5 +1,5 @@
 const Pago = require('../models/pagos');
-
+const nodemailer = require("nodemailer");
 
 async function getPago(req, res) {
   const { pagina = 1, limit = 10 } = req.query;
@@ -35,15 +35,53 @@ async function agregarPago(req, res) {
   
   const pago = new Pago({
 		idPago: req.body.idPago,   
-    idUser: req.body.pagoName,
+    idUser: req.body.idUser,
     idManga: req.body.idManga,
     total: Number(req.body.total),
     precio: Number(req.body.precio),
   })
 	await pago.save()
 	res.send(pago)
-  console.log(pago)
-  }
+  console.log(pago);
+  console.log(pago.idManga);
+  console.log(pago.idPago);
+  console.log(pago.idUser);
+  console.log(pago.total);
+  console.log(pago.precio);
+  var transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    post: 8080,
+    secure: false,
+    auth:{
+        user: "maximillian.torphy93@ethereal.email", /** el usuario y pass generado por Ethereal Email */
+        pass:  "JBhtefHZ7YdNwTBz15",
+    },
+});
+
+var mailOptions = {
+    from: "Tienda de Mangas",
+    to: "sevillanito435@gmail.com",
+    subject: `Recibo pedido ${pago.idPago}`,
+    text: `
+    Precio manga : ${pago.precio}
+    Total : ${pago.total}
+    
+    Gracias por su Compra ^^
+    `
+}
+
+transporter.sendMail(mailOptions, (err, info) => {
+    if(err){
+        res.status(500).send(err.message);
+    }
+    else{
+        console.log("Email ha sido enviado!!! :DDD");
+        res.status(200).json(req.body);
+    }
+})
+console.log('Email enviado');
+}
+  
   module.exports = {
     getPago,
     agregarPago
